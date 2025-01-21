a63be71d1a83849442ffc3c2cc65da2927df2762
DAVIS <UPN@UCLAB1328>
2025-01-21 12:04:49 -0500

    avance 2

--- Pilas/pila.cs
+++ Pilas/pila.cs
@@ -8,5 +8,27 @@ namespace Pilas
 {
     internal class pila
     {
+        private nodo cima = null;
+        private int contador = 0;
+
+        public void push(string nom)
+        {
+         nodo nuevo = new nodo();
+         nuevo.Nombre = nom;
+
+            nuevo.Siguiente = cima;
+            cima = nuevo;
+            contador++;
+
+        }
+        public void mostrar(ListBox list)
+        {
+            nodo actual = cima;
+            while (actual != null)
+            {
+                list.item.add(actual.Nombre);
+                actual=actual.Siguiente;
+            }
+        }
     }
 }
