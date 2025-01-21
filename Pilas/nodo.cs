13d6a1e90eb8565c8a15899d1b06f0dce1719cd9
DAVIS <UPN@UCLAB1328>
2025-01-21 11:26:55 -0500

    avance 1

---
+++ Pilas/nodo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilas
+{
+    internal class nodo
+    {
+        private string nombre;
+        private nodo siguiente;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        internal nodo Siguiente { get => siguiente; set => siguiente = value; }
+    }
+}
